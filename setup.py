fe9f77ee054fd9d8588975113153218c78ba5153
Ashesh Bajracharya <bajracharyaashesh12@gmail.com>
2026-03-04 14:40:15 +0545

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_qa',
+    version='0.1.0',
+    author='Ashesh Bajracharya',
+    author_email='bajracharyaashesh12@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
